47804d765aa2f3e77dd886f534b18a57309c51c8
Timothy Baldridge <tbaldridge@gmail.com>
2011-11-06 00:55:18 -0500

    new ::stop system seems to be working

--- src/reactive_clojure/core.clj
+++ src/reactive_clojure/core.clj
@@ -1,24 +1,24 @@
 (ns reactive-clojure.core
+    (:use [clojure.core.match :only [match]])
     (:import (java.util.concurrent ConcurrentLinkedQueue Executor))
     (:import (java.util UUID)))
 
 
 (defrecord NState [state f listeners])
 
-(defrecord NListener [filter-fn stopf-fn])
+(defn null-fn [state k v]
+    state)
  
 (defn make-node [f & opts]
     (let [{:keys [state]} (apply hash-map opts)
           ns (NState. state f {})]
          (agent ns)))
 
-(defn connect [from filter-fn stopf-fn to]
+(defn connect [from filter-fn to]
     (send from
-          #(assoc-in %1 [:listeners %2] 
-              (NListener. %3 %4))
+          #(assoc-in %1 [:listeners %2] %3)
            to
-           filter-fn
-           stopf-fn)
+           filter-fn)
     (await from))
 
 (defn emit 
@@ -27,12 +27,8 @@
              (fn [state k v] 
                  ((:f state) state k v))
              k v))
-    ([node listener k v]
-        (emit node (:filter-fn listener) (:stopf-fn listener) k v))
-    ([node filter-fn stopf-fn k v]
-        (if (= k :stop)
-            (emit node (stopf-fn k) v)
-            (emit node (filter-fn k) v))))
+    ([node filter-fn k v]
+        (emit node (filter-fn k) v)))
 
 (defn emit-all [state k v]
     (doseq [[node fns] (:listeners state)]
@@ -46,9 +42,12 @@
 
 (defn r-filter [f]
     (make-node (fn [state k v]
-                   (if (f v)
-                       (emit-all state k v))
-                   state)))
+                   (match [k v]
+                       [:default _] (if (f v)
+                                    (do (emit-all state k v))
+                                        state)
+                       [:default ::stop] (NState. nil null-fn nil)
+                       [_ _] state))))
                        
                         
            
--- test/reactive_clojure/test/core.clj
+++ test/reactive_clojure/test/core.clj
@@ -1,64 +1,24 @@
 (ns reactive-clojure.test.core
-  (:use [reactive-clojure.core]
+  (:use [reactive-clojure.core :as r]
   	    [reactive-clojure.util])
   (:use [clojure.test])
   (:import [java.lang.Thread]))
 
+
 (deftest filter-test
-	(let [f (register (rfilter #(= % 1)))
-		  a (rderef)
-		  p (register a )]
-		  (connect f p)
-		  (is (= @a nil))
-		  (update-slot f :default 2)
-		  (is (= @a nil))
-		  (update-slot f :default 1)
-		  (println (show-state))
-		  (is (= @a 1))))
-
-;(deftest atom-tests
-;	(let [a (atom 5)]
-;		 (push-change a 6)
-;		 (is (= @a 6))))
-; 
-; (deftest ratom-tests
-;	 (let [a (atom 5)
-;		   r (ratom a 4)]
-;		   (is (= @r 4))
-;		   (push-change a 7)
-;		   (is (= @r 7))))
-; 
- ; (deftest filter-tests
-	 ; (let [p (atom 0)
-	 	   ; fil (rfilter nil #(= % 1))]
-	 	   ; (add-subscriber fil :key #(swap! p (fn [_ _] %)) #(identity nil))
-		   ; (push-change fil 7)     
-		   ; (push-change fil 1)
-		   ; (Thread/sleep 100)
-		   ; (is (= @p 1))))
-
- ; (deftest map-tests
-	 ; (let [p (promise)
-	 	   ; r (rmap nil #(deliver p %1))]
-		   ; (push-change r 5)
-		   ; (is (= @p 5))
-		   ; ))
-; 
-; (deftest rskip-tests
-	; (let [m (rskip nil 2)
-		  ; r (ratom m nil)]
-		  ; (is (= @r nil))
-		  ; (push-change m 7)
-		  ; (is (= @r nil))
-		  ; (push-change m 7)
-		  ; (is (= @r nil))
-		  ; (push-change m 7)
-		  ; (is (= @r 7))))
-
-;(deftest pulse-tests
-;	(let [a (atom 5)
-;		  pulse (rpulse 10)]
-;		  (add-subscriber pulse a)
-;		  (Thread/sleep 1000)
-;		  (is (= @a nil))))
+    (let [a (atom nil)
+          d (r/r-do #(reset! a %2))
+          f (r/r-filter #(= % 1))]
+         (r/connect f identity d)
+         (is (= @a nil))
+         (r/emit f :default 1)  ; Only pass on matching events
+         (await f)
+         (is (= @a 1))
+         (r/emit f :default 2)
+         (is (= @a 1))
+         (r/emit f :default :r/stop) ; Don't pass anything after a :stop
+         (reset! a nil)
+         (r/emit f :default 1)
+         (is (= @a nil))))
+         
 
